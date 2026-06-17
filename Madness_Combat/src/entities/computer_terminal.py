188ef25ebedbb8bcff01b87e3101235b9f7baf49
Daniil Stopkevich <crepperpie8@mail.ru>
2026-06-17 19:23:45 +0300

    forgot to commit the termenal lmao

---
+++ Madness_Combat/src/entities/computer_terminal.py
@@ -0,0 +1,84 @@
+import os
+import pygame
+from ... import settings
+from .player import Player
+from ..systems.input_system import InputState
+from ..systems.collision_system import CollisionSystem
+from ..core.camera import Camera
+
+
+class ComputerTerminal:
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        width: int,
+        height: int,
+        sprite_path: str | None = None
+    ):
+        self.rect = pygame.Rect(x, y , width, height)
+
+        self.interaction_rect = pygame.Rect(
+            x - 40,
+            y + width + 120,
+            width + 80,
+            80
+        )
+        
+        self.sprite_path = sprite_path
+        self.sprite = None
+
+        if self.sprite_path is not None and os.path.exists(self.sprite_path):
+            self.sprite = pygame.image.load(self.sprite_path).convert_alpha()
+            self.sprite = pygame.transform.scale(self.sprite, (width, height))
+        
+        self.last_interact = False
+
+    def update(
+        self,
+        player: Player,
+        collision_system: CollisionSystem,
+        input_state: InputState
+    ) -> bool:
+        can_interact = self.can_player_interact(player, collision_system)
+
+        interact_pressed_once = input_state.interact and not self.last_interact
+        self.last_interact = input_state.interact
+
+        return can_interact and interact_pressed_once
+    
+    def can_player_interact(
+            self,
+            player: Player,
+            collsion_system: CollisionSystem
+    ) -> bool:
+        player_feet = collsion_system.get_player_feet_points(player.rect)
+
+        if self.interaction_rect.collidepoint(player_feet[0]):
+            return True
+        
+        if self.interaction_rect.collidepoint(player_feet[2]):
+            return True
+        
+        return False
+    
+    def render(
+        self,
+        screen,
+        camera: Camera = None
+    ):
+        rect = self.rect
+        interaction_rect = self.interaction_rect
+
+        if camera is not None:
+            rect = camera.apply(self.rect)
+            interaction_rect = camera.apply(self.interaction_rect)
+        
+        if self.sprite is not None:
+            screen.blit(self.sprite, rect)
+        else:
+            pygame.draw.rect(screen, (30, 30, 30), rect)
+            pygame.draw.rect(screen, settings.GREEN, rect, 3)
+        
+        pygame.draw.rect(screen, settings.GREEN, interaction_rect, 3)
+        
